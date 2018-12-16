ea3a7dad668a41c6610b6389c9c13d2995afbe35
YY <z3388638@gmail.com>
2018-12-16 12:25:12 +0800

    bind input string and output to caontainer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { InputBarComponent } from './input-bar/input-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InputBarComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		InputBarComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/input-bar/input-bar.component.ts
+++ src/app/input-bar/input-bar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-input-bar',
-  templateUrl: './input-bar.component.html',
-  styleUrls: ['./input-bar.component.css']
+	selector: 'app-input-bar',
+	templateUrl: './input-bar.component.html',
+	styleUrls: ['./input-bar.component.css']
 })
 export class InputBarComponent implements OnInit {
+	taskName: string;
+	@Output() onAdd = new EventEmitter<any>();
+	constructor() { }
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
-
+	handleAddTask(taskName) {
+		if (taskName !== undefined) {
+			this.onAdd.emit(taskName);
+		}
+	}
 }
