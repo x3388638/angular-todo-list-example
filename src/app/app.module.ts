b1ed0bf6bd5e13dee6387174725874087a35f5c5
YY <z3388638@gmail.com>
2018-12-16 11:22:56 +0800

    add input-bar & style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputBarComponent } from './input-bar/input-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/input-bar/input-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-bar',
+  templateUrl: './input-bar.component.html',
+  styleUrls: ['./input-bar.component.css']
+})
+export class InputBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
